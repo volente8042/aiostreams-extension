32e20419e523dd68831d30837db579df9fbb9c06
volente8042 <121372986+volente8042@users.noreply.github.com>
2026-07-11 20:56:40 +0900

    Update compiler version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,27 @@
-include(":core")
-include(":aiostreams")
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget as KotlinJvmTarget
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        
+        classpath("com.android.tools:r8:8.2.33")
+    }
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+        google()
+        maven(url = "https://jitpack.io")
+    }
+
+    tasks.withType<KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(KotlinJvmTarget.JVM_1_8)
+        }
+    }
+}
