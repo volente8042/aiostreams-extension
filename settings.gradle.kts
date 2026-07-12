275aaefc883f826bdb9a1602ef8ce734b56e207a
volente8042 <121372986+volente8042@users.noreply.github.com>
2026-07-12 12:07:00 +0900

    settings.gradle.kts fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,14 @@
+pluginManagement {
+    buildscript {
+        repositories {
+            google()
+            mavenCentral()
+        }
+        dependencies {
+            classpath("com.android.tools:r8:8.2.33")
+        }
+    }
+}
+
 include(":core")
 include(":aiostreams")
